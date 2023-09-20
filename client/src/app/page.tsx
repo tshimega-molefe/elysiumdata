ae21de6cbb9109e9fd334ed6a56f61427d15ffcb
tshimegabelmont <tshimegabelmont@gmail.com>
2023-09-20 14:02:53 +0200

    Feat: Added Clerk Provider

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1 className="text-red-600">hello world</h1>;
+  return (
+    <h1 className="text-red-600">
+      <Button>Click Me</Button>
+    </h1>
+  );
 }
