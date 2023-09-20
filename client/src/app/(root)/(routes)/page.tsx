8c5b20da9639fa86bd23ebdbb44f58415732c3bd
tshimegabelmont <tshimegabelmont@gmail.com>
2023-09-20 16:48:09 +0200

    Feat: DrizzleORM functional - NEON DB Pushed

--- client/src/app/(root)/(routes)/page.tsx
+++ client/src/app/(root)/(routes)/page.tsx
@@ -1,16 +1,18 @@
 import { Button } from "@/components/ui/button";
 import { UserButton, auth } from "@clerk/nextjs";
+import { LogIn } from "lucide-react";
+import Link from "next/link";
 
 export default async function Home() {
   const { userId } = await auth();
   const isAuthenticated = !!userId;
   return (
-    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+    <div className="w-screen min-h-screen">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
             <h1 className="mr-3 md:text-5xl text-2xl font-semibold">
-              Analyse any data:
+              Analyse financial data
             </h1>
             <UserButton afterSignOutUrl="/" />
           </div>
@@ -23,11 +25,22 @@ export default async function Home() {
             )}
           </div>
 
-          <p>
-            Instantly review integrated reports, financial statements, and
-            generate context aware analysis, recommendations, and risk
-            evaluations.
+          <p className="max-w-xl mt-1 text-lg text-muted-foreground">
+            Instantly review reports, financial statements, and generate
+            context-aware analysis, projections, and risk evaluations.
           </p>
+
+          <div className="w-full mt-4">
+            {isAuthenticated ? (
+              <Button>File Upload Component</Button>
+            ) : (
+              <Link href="/sign-in">
+                <Button variant="default" size="lg">
+                  Get Started <LogIn className="ml-2 h-4 w-4" />
+                </Button>
+              </Link>
+            )}
+          </div>
         </div>
       </div>
     </div>
