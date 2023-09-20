fb08b11e9cff924f58a0c28a14b2696fb5f77adc
tshimegabelmont <tshimegabelmont@gmail.com>
2023-09-20 18:05:17 +0200

    Added Progress to s3 fileUpload

--- client/src/lib/s3.ts
+++ client/src/lib/s3.ts
@@ -6,5 +6,27 @@ export async function uploadToS3(file: File) {
       accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
       secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY,
     });
+    const s3 = new AWS.S3({
+      params: {
+        Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
+      },
+      region: "eu-west-2",
+    });
+
+    const file_key =
+      "uploads/" + Date.now().toString() + file.name.replace(" ", "-");
+
+    const params = {
+      Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+      Key: file_key,
+      Body: file,
+    };
+
+    const upload = s3.putObject(params).on("httpUploadProgress", (evt) => {
+      console.log(
+        "Uploading to s3...",
+        parseInt(((evt.loaded * 100) / evt.total).toString())
+      );
+    });
   } catch (error) {}
 }
